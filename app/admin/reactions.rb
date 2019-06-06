9ad1e15eb60989d03792fc9979147f66d139c6c4
Raghu Betina <raghu@firstdraft.com>
2019-06-06 16:21:15 +0000

    Whitelist columns in Reaction for ActiveAdmin

--- app/admin/reactions.rb
+++ app/admin/reactions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Reaction do
+
+ permit_params :reaction, :user_id, :submission_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
