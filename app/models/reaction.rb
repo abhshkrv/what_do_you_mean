a46fabcda8fae4e6e18613857899ad3f39a5b2e2
Raghu Betina <raghu@firstdraft.com>
2019-06-06 16:20:32 +0000

    Direct association between users and reactions

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,8 @@
 class Reaction < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:reactions) }
+
     it { should have_many(:submissions) }
 
     end
