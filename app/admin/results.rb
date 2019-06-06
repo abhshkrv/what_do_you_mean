0ccd97fdeab57d2ad93f64fe6b7c8d1a1256aaa6
Raghu Betina <raghu@firstdraft.com>
2019-06-06 16:21:09 +0000

    Whitelist columns in Result for ActiveAdmin

--- app/admin/results.rb
+++ app/admin/results.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Result do
+
+ permit_params :submission_id, :flags, :confidence
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
